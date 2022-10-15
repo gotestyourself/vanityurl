9a7765325a766ef8c50dc5c7d4ccb7d8fc9ad54a
Daniel Nephin <dnephin@gmail.com>
2022-10-15 16:17:42 -0400

    Add new packages

--- render.sh
+++ render.sh
@@ -2,11 +2,14 @@
 set -eu -o pipefail
 
 rm -rf pkgs repos docs
+git checkout docs/CNAME
 mkdir -p repos docs
+target="$PWD/pkgs"
+
 pushd repos
 
 golist() {
-    go list -f '{{.Module.Path}} {{.ImportPath}}' ./... >> ../../pkgs
+    go list -f '{{.Module.Path}} {{.ImportPath}}' ./... >> "$target"
 }
 
 git clone --quiet git@github.com:gotestyourself/gotestsum
@@ -19,6 +22,7 @@ git clone --quiet git@github.com:gotestyourself/gotest.tools
 (
     cd gotest.tools
     golist
+    (cd x/generics; golist)
     git checkout --quiet v2.2.0
     go mod init gotest.tools
     golist
